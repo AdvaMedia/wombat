e27d01493ee082721a0ce0a70ff6be9a1eea6fa3
Kirillov Alexander <saratovsource@gmail.com>
2013-05-28 19:02:48 +0400

    Added advanced methods tests

--- spec/integration/various_request_methods_spec.rb
+++ spec/integration/various_request_methods_spec.rb
@@ -7,22 +7,29 @@ describe 'advanced crawler setup' do
       crawler = Class.new
       crawler.send(:include, Wombat::Crawler)
 
-      crawler.base_url "http://www.github.com"
-      crawler.path "/"
+      crawler.base_url "http://www.centpart.ru"
+      crawler.path "/new-media/select"
 
       crawler.request do
         http_method :post
-        http_headers {}
+        http_headers({'Host' => 'www.centpart.ru', 'Accept-Language' => 'ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4', 'X-Requested-With' => 'XMLHttpRequest'})
+        http_parameters do
+          page "2"
+        end
       end
 
-      crawler.headline xpath: "//h1"
-      crawler.subheading css: "p.subheading"
-
-      crawler.what_is({ css: ".teaser h3" }, :list)
+      crawler.videos "css=.ticket", :iterator do
+        id "xpath=@data-film_id"
+        data do
+          title "xpath=div[@class='descr']//b//a"
+          path "xpath=div[@class='descr']//b//a/@href"
+        end
+      end
 
       crawler_instance = crawler.new
 
       results = crawler_instance.crawl
+      puts results
     end
   end
 end
